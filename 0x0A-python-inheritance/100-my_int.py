690f8b2f19ba58d57bf615e010cbbd1445b36732
Emmanuel Adeniji <51063705+Fabuloz@users.noreply.github.com>
2023-04-11 23:53:05 +0100

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""
-begin program - python
-"""
+# 100-my_int.py
+"""Defines a class MyInt that inherits from int."""
 
 
 class MyInt(int):
-    def __eq__(self, y):
-        if self <= y and self >= y:
-            return False
-        return True
+    """Invert int operators == and !=."""
 
-    def __ne__(self, y):
-        if self > y or self < y:
-            return False
-        return True
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
